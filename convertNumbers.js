7285166292fe55ee5403e092d71293bca88d4f1e
AlexConfluDigital <alexandre@confluent-digital.com>
2021-03-19 17:10:40 +0100

    fixed convert

--- convertNumbers.js
+++ convertNumbers.js
@@ -19,12 +19,6 @@ function convertNumbers(convert) {
   }
 
 }
-const chiffres = [[1, 'I'], [2, 'II'], [3, 'III'], [4, 'IV'], [5, 'V'], [6, 'VI'], [7, 'VII'], [8, 'VIII'], [9, 'IX'], [10, 'X'], [20, 'XX'], [30, 'XXX'], [40, 'XL'], [50, 'L']]
 
-chiffres.forEach((arabe, romain) => {
-  exp()
-  console.log(item) //value
-  console.log(index) //index
-})
 
 module.exports = convertNumbers;
